8916ae6a7ced2797236fd0c083b0fd4b91f931b4
Artem <xfan08@mail.ru>
2025-03-15 22:32:04 +0800

    fixed all problems, i hope...

--- src/components/TaskMenu/index.tsx
+++ src/components/TaskMenu/index.tsx
@@ -1,77 +1,77 @@
-import { FormEvent, useState } from 'react'
-import { TouchLine } from '../TouchLine'
-import css from './index.module.scss'
-import { MainButton } from '../Button'
+// import { FormEvent, useState } from 'react'
+// import { TouchLine } from '../TouchLine'
+// import css from './index.module.scss'
+// import { MainButton } from '../Button'
 
-export const TaskMenu = () => {
-  const [isActive, setIsActive] = useState(true)
-  const [value, setValue] = useState({
-    name: '',
-    time: {
-      seconds: 0,
-      hours: 0,
-      minutes: 0,
-    },
-  })
+// export const TaskMenu = () => {
+//   const [isActive, setIsActive] = useState(true)
+//   const [value, setValue] = useState({
+//     name: '',
+//     time: {
+//       seconds: 0,
+//       hours: 0,
+//       minutes: 0,
+//     },
+//   })
 
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault()
+//   const handleSubmit = (e: FormEvent) => {
+//     e.preventDefault()
 
-    console.log(value)
-  }
+//     console.log(value)
+//   }
 
-  if (isActive) {
-    return (
-      <div className={css.taskMenu}>
-        <div className={css.content}>
-          <form onSubmit={handleSubmit} className={css.form}>
-            <input
-              type="text"
-              name="name"
-              placeholder="task name"
-              onChange={(e) => setValue({ ...value, name: e.target.value })}
-              value={value.name}
-              className={css.input}
-            />
+//   if (isActive) {
+//     return (
+//       <div className={css.taskMenu}>
+//         <div className={css.content}>
+//           <form onSubmit={handleSubmit} className={css.form}>
+//             <input
+//               type="text"
+//               name="name"
+//               placeholder="task name"
+//               onChange={(e) => setValue({ ...value, name: e.target.value })}
+//               value={value.name}
+//               className={css.input}
+//             />
 
-            <input
-              type="text"
-              name="name"
-              placeholder="task name"
-              onChange={(e) => setValue({ ...value, time: { ...value.time, hours: Number(e.target.value) } })}
-              value={value.time.hours}
-              className={css.input}
-            />
-            <input
-              type="text"
-              name="name"
-              placeholder="task name"
-              onChange={(e) => setValue({ ...value, time: { ...value.time, minutes: Number(e.target.value) } })}
-              value={value.time.minutes}
-              className={css.input}
-            />
-            <input
-              type="text"
-              name="name"
-              placeholder="task name"
-              onChange={(e) => setValue({ ...value, time: { ...value.time, seconds: Number(e.target.value) } })}
-              value={value.time.seconds}
-              className={css.input}
-            />
+//             <input
+//               type="text"
+//               name="name"
+//               placeholder="task name"
+//               onChange={(e) => setValue({ ...value, time: { ...value.time, hours: Number(e.target.value) } })}
+//               value={value.time.hours}
+//               className={css.input}
+//             />
+//             <input
+//               type="text"
+//               name="name"
+//               placeholder="task name"
+//               onChange={(e) => setValue({ ...value, time: { ...value.time, minutes: Number(e.target.value) } })}
+//               value={value.time.minutes}
+//               className={css.input}
+//             />
+//             <input
+//               type="text"
+//               name="name"
+//               placeholder="task name"
+//               onChange={(e) => setValue({ ...value, time: { ...value.time, seconds: Number(e.target.value) } })}
+//               value={value.time.seconds}
+//               className={css.input}
+//             />
 
-            <MainButton type={"submit"} >Add</MainButton>
-          </form>
-        </div>
-        <div>
-          <TouchLine setOpen={setIsActive} isOpen={isActive} />
-        </div>
-      </div>
-    )
-  } else {
-    return (
-      <div className={css.touchLine}>
-        <TouchLine setOpen={setIsActive} isOpen={isActive} />
-      </div>
-    )
-  }
-}
+//             <MainButton type={"submit"} >Add</MainButton>
+//           </form>
+//         </div>
+//         <div>
+//           <TouchLine setOpen={setIsActive} isOpen={isActive} />
+//         </div>
+//       </div>
+//     )
+//   } else {
+//     return (
+//       <div className={css.touchLine}>
+//         <TouchLine setOpen={setIsActive} isOpen={isActive} />
+//       </div>
+//     )
+//   }
+// }
